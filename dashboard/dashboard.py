ef27022ef6e49a9f136ed3b0eb72eed8bf659133
muditaindah <84949681+muditaindah@users.noreply.github.com>
2024-10-07 02:07:50 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,11 +6,9 @@ import seaborn as sns
 import os
 import geopandas as gpd
 
-os.chdir('./KualitasUdara')
-
 image_url = "https://github.com/muditaindah/KualitasUdara/blob/main/dashboard/beijing.jpg"
 st.write("<h1 style='text-align: center; color: #3498db'>Air Quality Analysis of Beijing</h1>", unsafe_allow_html=True)
-st.image("image_url", use_column_width=True)
+st.image('image_url', use_column_width=True)
 
 big_df = pd.read_csv('compilation_data.csv')
 big_df_clean = big_df.drop_duplicates()
